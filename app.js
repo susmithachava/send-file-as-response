2d37c37332471d9fca624c2fe75eba06e35de9e8
root <susmi.chava@gmail.com>
2021-07-09 07:14:03 +0000

    send file as response

---
+++ app.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const app = express();
+
+module.exports = app.get("/gadgets", (request, response) => {
+  response.sendFile("/gadgets.html", { root: __dirname });
+});
